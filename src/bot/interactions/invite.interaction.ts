47ffcc27aac8620103fe34852ae729b8bb21fe46
Armagidosha <armagidoshkaa@gmail.com>
2025-08-16 19:34:51 +0300

    fix: change stringSelect to userSelect

--- src/bot/interactions/invite.interaction.ts
+++ src/bot/interactions/invite.interaction.ts
@@ -3,12 +3,12 @@ import {
   ActionRowBuilder,
   ButtonInteraction,
   PermissionsBitField,
-  StringSelectMenuBuilder,
-  StringSelectMenuInteraction,
+  UserSelectMenuBuilder,
+  UserSelectMenuInteraction,
 } from 'discord.js';
 import { CONFIG, INP_CONTENT, MESSAGES } from 'src/common/constants';
-import { CheckRightsService } from '../checkRights.service';
-import { InteractionExtractorService } from '../interactionExtractor.service';
+import { CheckRightsService } from '../extra/checkRights.service';
+import { InteractionExtractorService } from '../extra/interactionExtractor.service';
 
 @Injectable()
 export class InviteInteraction {
@@ -28,19 +28,10 @@ export class InviteInteraction {
       return;
     }
 
-    const { voiceChannel, userId, guild, guildMembers } =
+    const { voiceChannel, guild } =
       await this.interactionExtractor.extract(interaction);
 
     const everyone = guild.roles.everyone;
-
-    const selectOptions = guildMembers
-      .filter((memb) => !memb.user.bot)
-      .filter((memb) => memb.id !== userId)
-      .map((memb) => ({ label: memb.displayName, value: memb.id }));
-
-    if (!selectOptions.length) {
-      await interaction.editReply(MESSAGES.SERVER_ALONE);
-    }
 
     if (
       voiceChannel
@@ -51,11 +42,10 @@ export class InviteInteraction {
       return;
     }
 
-    const row = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
-      new StringSelectMenuBuilder()
+    const row = new ActionRowBuilder<UserSelectMenuBuilder>().addComponents(
+      new UserSelectMenuBuilder()
         .setCustomId(CONFIG.INP_INVITE)
-        .setPlaceholder(INP_CONTENT.invite_select)
-        .addOptions(selectOptions),
+        .setPlaceholder(INP_CONTENT.invite_select),
     );
 
     await interaction.editReply({
@@ -64,14 +54,30 @@ export class InviteInteraction {
     });
   }
 
-  async onInputInteract(interaction: StringSelectMenuInteraction) {
+  async onInputInteract(interaction: UserSelectMenuInteraction) {
     await interaction.deferReply({ flags: 'Ephemeral' });
 
-    const { voiceChannel, userId, guild, values } =
+    const { voiceChannel, user, userId, guild, values, guildMembers } =
       await this.interactionExtractor.extract(interaction);
 
     const selectedUserId = values[0];
     const invitedUser = await guild.members.fetch(selectedUserId);
+    const isBot = user.bot;
+
+    if (invitedUser.id === userId || isBot) {
+      await interaction.editReply(MESSAGES.SELF_OR_BOT_SELECTED);
+      return;
+    }
+
+    const membersId = voiceChannel.members.map((user) => user.id);
+    const filterInChannel = guildMembers.filter((memb) =>
+      membersId.includes(memb.id),
+    );
+
+    if (filterInChannel.has(userId)) {
+      await interaction.editReply(MESSAGES.CANT_INVITE_USER_IN_CHANNEL);
+      return;
+    }
 
     let reply = `<@${invitedUser.id}> теперь может посещать твой канал.`;
 
